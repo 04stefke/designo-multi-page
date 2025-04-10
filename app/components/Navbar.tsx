2d462dca6dc0a1acd38f4ab465d96df4a2707c6f
04stefke <jovanovics399@yahoo.com>
2025-04-10 17:01:10 +0100

    created header structure, added the dropdown icon and styled the logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+	return (
+		<header className="px-5 py-10 flex items-center justify-between ">
+			<img src="assets/shared/desktop/logo-dark.png" alt="" className="w-44" />
+			<nav>
+				<ul className="hidden xl:flex items-center gap-5">
+					<li className="p-3">a</li>
+					<li className="p-3">a</li>
+					<li className="p-3">a</li>
+				</ul>
+				<img src="assets/shared/mobile/icon-hamburger.svg" alt="" className="block xl:hidden" />
+			</nav>
+		</header>
+	);
+};
 
-export default Navbar
+export default Navbar;
