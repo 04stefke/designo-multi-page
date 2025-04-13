c357b86a4a2f28dbdbafeaea2a4c0a19c9f0164d
04stefke <jovanovics399@yahoo.com>
2025-04-13 17:56:45 +0100

    styled the image and added responsiveness for all devices

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<main>
-			<section className="bg-peach-c text-white flex flex-col items-center gap-10 homePage-bg-image pt-20 relative h-[650px] overflow-hidden">
+		<main className="flex flex-col items-center justify-center">
+			<section className="bg-peach-c text-white flex flex-col items-center lg:justify-between gap-10 homePage-bg-image pt-20 max-lg:h-[650px] md:w-[90%] overflow-hidden">
 				<div className=" flex flex-col items-center justify-center text-center gap-5 max-w-[90%]">
 					<h2 className="text-2xl font-bold">
 						Award-winning custom designs and digital branding solutions
@@ -15,7 +15,13 @@ export default function Home() {
 						LEARN MORE
 					</button>
 				</div>
-				<img src="assets/home/desktop/image-hero-phone.png" alt="phone image" className="absolute bottom-[-220px] " />
+				<div className="relative w-full flex justify-center">
+					<img
+						src="assets/home/desktop/image-hero-phone.png"
+						alt="phone image"
+						className="max-lg:absolute bottom-[-400px] xs:bottom-[-500px] 2xs:bottom-[-600px] 3xs:bottom-[-750px] sm:bottom-[-800px] md:bottom-[-800px] transition-all duration-700 ease-in-out"
+					/>
+				</div>
 			</section>
 		</main>
 	);
