2f52ba3886c9e2254ce4f35d3db7104d4f4290fc
04stefke <jovanovics399@yahoo.com>
2025-04-13 16:49:55 +0100

    starting creating and styling home page, added first section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
-
 export default function Home() {
-	return <></>;
+	return (
+		<main>
+			<section className="bg-peach-c text-white flex flex-col items-center justify-center gap-10">
+				<div className=" flex flex-col items-center justify-center text-center gap-5 max-w-[90%]">
+					<h2 className="text-2xl font-bold">Award-winning custom designs and digital branding solutions</h2>
+					<p className="text-sm">
+						With over 10 years in the industry, we are experienced in creating
+						fully responsive websites, app design, and engaging brand
+						experiences. Find out more about our services.
+					</p>
+					<button className="bg-white-c rounded-lg h-[56px] w-[150px] text-darkGray-c cursor-pointer hover:brightness-75">LEARN MORE</button>
+				</div>
+				<img src="" alt="" />
+			</section>
+		</main>
+	);
 }
